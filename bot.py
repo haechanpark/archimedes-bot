ec2f8878573ba6b348874c08cd9cad35988bbd79
haechanpark <32949283+haechanpark@users.noreply.github.com>
2020-01-28 20:45:35 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,12 +3,17 @@ import discord
 from discord.ext import commands
 from pyson import pyson
 
-currency=pyson('currency')
+brittany=pyson('brittany')
+soldier=pyson('infantry')
 
-if 'name' not in currency.data:
-    currency.data['name']='dollars'
+if 'name' not in soldier.data:
+    soldier.data['name']='infantry'
 
-bot=commands.Bot(command_prefix="++")
+if 'name' not in brittany.data:
+    brittany.data['name']='brittany'
+
+
+bot=commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
@@ -16,9 +21,14 @@ async def on_ready():
     print('With user ID: '+bot.user.id)
 
 def check_id(ID):
-    if ID not in currency.data:
-        currency.data[ID]=0
-        currency.save()
+    if ID not in soldier.data:
+        soldier.data[ID]=0
+        soldier.save()
+
+def check_ID2(ID):
+    if ID not in brittany.data:
+        brittany.data[ID]=0
+        brittany.save()
 
 def is_approved():
     def predicate(ctx):
@@ -29,45 +39,47 @@ def is_approved():
     return commands.check(predicate)   
 
 @is_approved()
-@bot.command()
-async def currency_type(ntype:str='dollars'):
-    ''': Change name of your currency'''
-    ptype=currency.data['name']
-    currency.data['name']=ntype
-    currency.save()
-    await bot.say(f'The economy type has been changed from **{ptype}** to **{ntype}**')
+@bot.command(pass_context=True)
+async def add(ctx,amount:int=0,member:discord.Member=None):
+    ''': Add infantry to a member's army'''
+    
+    ID=member.id
+    check_id(ID)
+    soldier.data[ID]+=amount
+    soldier.save()
+    await bot.say(f'''{amount} {soldier.data["name"]} have been added to {member.mention}'s army''')
 
 @is_approved()
 @bot.command(pass_context=True)
-async def add(ctx,amount:int=0,member:discord.Member=None):
-    ''': Add points/currency to a member's stash'''
+async def addbrittany(ctx,amount:int=0,member:discord.Member=None):
+    ''': Add infantry to a member's army'''
     ID=member.id
-    check_id(ID)
-    currency.data[ID]+=amount
-    currency.save()
-    await bot.say(f'''{amount} {currency.data["name"]} have been added to {member.mention}'s stash''')
+    check_ID2(ID)
+    brittany.data[ID]+=amount
+    brittany.save()
+    await bot.say(f'''{amount} {brittany.data["name"]} have been added to {member.mention}'s army''')
 
 @is_approved()
 @bot.command(pass_context=True)
 async def remove(ctx,amount:int=0,member:discord.Member=None):
-    ''': Remove points/currency from a member's stash'''
+    ''': Remove infantry from a member's army'''
     ID=member.id
     check_id(ID)
-    currency.data[ID]-=amount
-    currency.save()
-    await bot.say(f'''{amount} {currency.data["name"]} has been removed from {member.mention}'s stash''')
+    soldier.data[ID]-=amount
+    soldier.save()
+    await bot.say(f'''{amount} {soldier.data["name"]} has been removed from {member.mention}'s army''')
 
 @bot.command(pass_context=True)
-async def stash(ctx):
-    ''': Check your stash!'''
+async def totalarmy(ctx):
+    ''': Check your army!'''
     member=ctx.message.author
     check_id(member.id)
-    await bot.reply(f'you have {currency.data[member.id]} {currency.data["name"]}')
+    await bot.reply(f'you have {soldier.data[member.id]} {soldier.data["name"]}')
 
 @bot.command(aliases=['leaderboards'])
 async def leaderboard():
     ''': View the server leaderboad'''
-    members=[(ID,score) for ID,score in currency.data.items() if ID !='name']
+    members=[(ID,score) for ID,score in soldier.data.items() if ID !='name']
     if len(members)==0:
         await bot.say('I have nothing to show')
         return
@@ -84,4 +96,4 @@ async def leaderboard():
     await bot.say(embed=embed)
             
 
-bot.run('TOKEN')
+bot.run('tokem')
